06387d1c190bb0cd873d8c01e15ddc9ca4af5084
OmarAhmed0001 <oa1476@fayoum.edu.eg>
2023-07-11 19:26:20 +0300

    user

--- routes/wishlistRoute.ts
+++ routes/wishlistRoute.ts
@@ -1,20 +1,15 @@
 import express from 'express';
-import { addProductToWishlist } from '../controllers/WishlistController';
+import {
+    addProductToWishlist,
+    removeProductFromWishlist,
+} from '../controllers/WishlistController';
 
 import { protect, allowedTo } from '../controllers/AuthController';
 
 const router = express.Router();
 
+router.route('/').post(protect, allowedTo('user'), addProductToWishlist);
 router
-    .route('/')
-    .post(protect, allowedTo('user'), addProductToWishlist);
-// router
-//     .route('/:id')
-//     .get(getBrandValidator, getBrand)
-//     .put(
-//         protect,
-//         allowedTo('admin', 'manager'),
-//         updateBrand
-//     )
-//     .delete(protect, allowedTo('admin'), deleteBrandValidator, deleteBrand);
+    .route('/:productId')
+    .delete(protect, allowedTo('user'), removeProductFromWishlist);
 export default router;
